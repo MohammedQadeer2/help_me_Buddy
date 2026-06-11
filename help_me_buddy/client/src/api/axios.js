43778bc3cd33b3729638d312a1b65fc4c8fef5b0
mohammed Qadeer <mr0060195@gmail.com>
2026-06-11 17:55:03 -0600

    changing the urls from local to production

--- help_me_buddy/client/src/api/axios.js
+++ help_me_buddy/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://help-me-buddy.onrender.com/api",
+  baseURL: "https://help-me-buddy-backend.onrender.com/api",
   // baseURL: "http://localhost:5000/api",
   withCredentials: true, // This allows cookies to be sent back and forth securely
 });
